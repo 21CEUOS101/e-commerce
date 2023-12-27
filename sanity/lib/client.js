17d1f363419d6f332eeb3624277538da3ad2af70
21CEUOS101 <21ceuos101@ddu.ac.in>
2023-12-27 20:53:08 +0530

    haji thay chhe

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -1,10 +1,15 @@
 import { createClient } from 'next-sanity'
+import imageUrlBuilder from '@sanity/image-url';
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  useCdn,
-})
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: '2021-03-25',
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
